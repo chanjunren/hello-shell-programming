820dd5ed02debac3c1207af1cd0004730967851e
chan_junren <chanjunren@gmail.com>
2021-12-22 17:47:11 +0800

    printf example

---
+++ linux-and-shell-basics/printfexample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+printf "%-10s%-10s%12s\n" "No" "Move" "Difficulty"
+printf "%-10s%-10s%12.2f\n" "1" "Forward Toe Seven" "30"
+printf "%-10s%-10s%12.2f\n" "2" "Backward Toe Seven" "999.0"
+printf "%-10s%-10s%12.2f\n" "3" "Heel Fake" "45.0"
